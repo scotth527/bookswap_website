22f3bdce445a67360f98f6becc8b3fd864f819ca
Carlos Rivera <crivera2596@gmail.com>
2018-10-31 22:30:32 +0000

    Library

--- src/js/component/library/item.jsx
+++ src/js/component/library/item.jsx
@@ -3,74 +3,76 @@ import PropTypes from "prop-types";
 
 export class Item extends React.Component {
 	constructor(props) {
-		super();
+		super(props);
+
+		this.toggle = this.toggle.bind(this);
 		this.state = {
-			newItem: props.newItem
+			toggle: false
 		};
 	}
-	render() {
-		if (this.state.newItem) {
-			return (
-				<React.Fragment>
-					<button
-						className="btn btn-outline-dark btn-lg btn-block mb-3 border border-dark rounded"
-						style={{ whiteSpace: "normal" }}>
-						<div className="row">
-							<div className="col-2 my-auto">
-								<img src="https://via.placeholder.com/75x75" />
-							</div>
-							<div className="col-10 mx-auto my-auto">
-								<h4>Add a Book</h4>
-							</div>
-						</div>
-					</button>
 
-					<hr className="featurette-divider my-3" />
-				</React.Fragment>
-			);
-		} else {
+	toggle() {
+		this.setState(prevState => ({
+			toggle: !prevState.toggle
+		}));
+	}
+
+	shorten(description) {
+		if (description.length > 150) {
 			return (
-				<div className="col-12 bg-light border border-dark rounded">
-					<div className="row">
-						<div className="col-2 my-auto">
-							<img src="https://via.placeholder.com/75x75" />
+				description.substring(
+					0,
+					description.substring(0, 200).lastIndexOf(" ")
+				) + "..."
+			);
+		} else return description;
+	}
+
+	render() {
+		return (
+			<div className="col-12 bg-light border border-dark rounded mb-2">
+				<div className="row">
+					<div className="col-2 my-auto">
+						<img src="https://via.placeholder.com/75x75" />
+					</div>
+					<div className="col-10 py-2">
+						<div className="row">
+							<div className="col-8" alt="Title">
+								<h5>{this.props.title}</h5>
+							</div>
+							<div className="col-4 text-right" alt="Rating">
+								<i className="far fa-star" />
+								<i className="far fa-star" />
+								<i className="far fa-star" />
+								<i className="far fa-star" />
+								<i className="far fa-star" />
+							</div>
 						</div>
-						<div className="col-10 py-2">
-							<div className="row">
-								<div className="col-8" alt="Title">
-									Placeholder
-								</div>
-								<div className="col-4 text-right" alt="Rating">
-									<i className="far fa-star" />
-									<i className="far fa-star" />
-									<i className="far fa-star" />
-									<i className="far fa-star" />
-									<i className="far fa-star" />
-								</div>
+						<div className="row">
+							<div className="col-12" alt="Description">
+								<p>{this.shorten(this.props.description)}</p>
 							</div>
-							<div className="row">
-								<div className="col-12" alt="Description">
-									PLACEHOLDER
-								</div>
-							</div>
-							<div className="row justify-content-end pr-2">
-								<button
-									className="btn"
-									style={{ whiteSpace: "normal" }}>
-									View Book Page
-								</button>
-							</div>
+						</div>
+						<div className="row justify-content-end pr-2">
+							<button
+								className="btn"
+								style={{ whiteSpace: "normal" }}>
+								View Book Page
+							</button>
 						</div>
 					</div>
 				</div>
-			);
-		}
+			</div>
+		);
 	}
 }
 Item.propTypes = {
-	newItem: PropTypes.boolean
+	title: PropTypes.string,
+	description: PropTypes.string
 };
-Item.defaultTypes = {
-	newItem: false
+Item.defaultProps = {
+	title: "Placeholder",
+	description: "PLACEHOLDER"
 };
+
 export default Item;
