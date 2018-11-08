831564763a0f1a0c772edc11d5f8136818346976
FrankMan <fmanzanarez@outlook.com>
2018-11-08 16:41:17 +0000

    pushing the books

--- src/js/views/wishlist.jsx
+++ src/js/views/wishlist.jsx
@@ -26,28 +26,30 @@ export class Wishlist extends React.Component {
 						<Item />
 						<Item />
 						<SearchModal
-		                    show={this.state.showModal}
-		                    onClose={() => this.setState({ showModal: false })}
-		                />
-		                <div className="row mt-5 text-right">
-			                    <div
-			                        style={{
-			                            position: "fixed",
-			                            bottom: "20px",
-			                            overflow: "hidden",
-			                            height: "100px",
-			                            marginTop: "30px"
-			                        }}
-			                        className=" col-12 mx-auto ">
-			                        <i
-			                            id="addButton"
-			                            onClick={() => this.setState({ showModal: true })}
-			                            style={{
-			                                fontSize: "3em"
-			                            }}
-			                            className="btn far fa-plus-square"
-			                        />
-			                    </div>
+							show={this.state.showModal}
+							onClose={() => this.setState({ showModal: false })}
+						/>
+						<div className="row mt-5 text-right">
+							<div
+								style={{
+									position: "fixed",
+									bottom: "20px",
+									overflow: "hidden",
+									height: "100px",
+									marginTop: "30px"
+								}}
+								className=" col-12 mx-auto ">
+								<i
+									id="addButton"
+									onClick={() =>
+										this.setState({ showModal: true })
+									}
+									style={{
+										fontSize: "3em"
+									}}
+									className="btn far fa-plus-square"
+								/>
+							</div>
 						</div>
 					</div>
 				</div>
